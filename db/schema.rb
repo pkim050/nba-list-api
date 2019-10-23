d587b6040515d37b6fc25bee01d4f9f413ac10c5
pkim050 <pkim050@gmail.com>
2019-10-23 08:52:20 -0700

    added likes to players

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_02_051935) do
+ActiveRecord::Schema.define(version: 2019_10_23_154922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_10_02_051935) do
     t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes", default: 0
   end
 
   create_table "teams", force: :cascade do |t|
